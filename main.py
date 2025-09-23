1cd853511f0c97e666a211aaac115fa6b39f6517
Akinkunmi Oyeyemi <akinkunmioyeyemiola@gmail.com>
2025-09-23 23:08:40 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,62 @@
+import getpass
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+if not os.environ.get('groq_api_key'):
+    os.environ['groq_api_key'] = getpass.getpass('Enter your Groq API key: ')
+
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
+from langgraph.graph import START, END, StateGraph, MessagesState
+from langgraph.checkpoint.memory import MemorySaver
+
+from pydantic import BaseModel
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse, JSONResponse
+
+app = FastAPI()
+
+class chat(BaseModel):
+    text:str
+
+model = ChatGroq(model="openai/gpt-oss-20b", api_key=os.environ['groq_api_key'], temperature=0)
+
+prompt = ChatPromptTemplate.from_messages([
+    SystemMessagePromptTemplate.from_template("""
+    "Your are a chat assistant name Blossoms. You answer user questions in a friendly tone"
+    "When a user messages you, mention your name and that you are happy to help them."
+    "Tell user that you love to know their name"
+"""   ),
+    HumanMessagePromptTemplate.from_template("{text}"
+    )
+])
+
+def call_model(state:dict):
+    response = model.invoke(state["messages"])
+    return {"messages":response}
+
+workflow = StateGraph(state_schema=MessagesState)
+
+memory = MemorySaver()
+workflow.add_node("model", model)
+workflow.add_node("call_model", call_model)
+
+workflow.add_edge(START, "call_model")
+workflow.add_edge("call_model", END)
+
+graph = workflow.compile(checkpointer=memory)
+
+@app.get("/")
+async def root(text:str):
+    messages = prompt.format_messages(
+        text=text)
+    response = graph.invoke(
+        {"messages":messages},
+        config={
+            "configurable": {"thread_id": "user-1"}
+        })
+    ai_res = response["messages"][-1]
+    return {"response":ai_res.content}
+  
